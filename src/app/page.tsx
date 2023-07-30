63c5b512aad0db42b3a2eb224ace68d0a351c248
willolsker <willolsker3.14@gmail.com>
2023-07-29 18:04:11 -0700

    feat: add UI skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+        <div className={styles.leftHalf}>
+            <div className={styles.mixPanels}>
+                <div className={styles.mixPanel}>
+                </div>
+                <div className={styles.mixPanel}>
+                </div>
+            </div>
+            <div className={styles.controlBar}>
 
+            </div>
+        </div>
+        <div className={styles.playlistBar}>
+            <div className={styles.title}>PLAYLIST</div>
+        </div>
     </main>
   )
 }
